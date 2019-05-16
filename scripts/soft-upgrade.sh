958393223649724e021fa271f78e84aff3d0e799
TaKouMyS <allan38430@free.fr>
2019-05-16 16:57:58 +0200

    Update soft-upgrade.sh

--- scripts/soft-upgrade.sh
+++ scripts/soft-upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-printf "\n$(date)\n" | tee /var/log/update_script.log
-printf "apt-get update :\n\n" | tee /var/log/update_script.log
-apt-get update -y | tee /var/log/update_script.log
-printf "\napt-get upgrade :\n\n" | tee /var/log/update_script.log
-apt-get upgrade -y | tee /var/log/update_script.log
-printf "<---------- Done ----------->\n" | tee /var/log/update_script.log
+printf "\n$(date)\n" | tee -a /var/log/update_script.log
+printf "apt-get update :\n\n" | tee -a /var/log/update_script.log
+apt-get update -y | tee -a /var/log/update_script.log
+printf "\napt-get upgrade :\n\n" | tee -a /var/log/update_script.log
+apt-get upgrade -y | tee -a /var/log/update_script.log
+printf "<---------- Done ----------->\n" | tee -a /var/log/update_script.log
